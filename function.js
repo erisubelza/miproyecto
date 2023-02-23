bb9f65a237fafae2f6301012b371d482d8eb16be
EricaSubelza <124851446+erisubelza@users.noreply.github.com>
2023-02-23 13:37:54 -0300

    comentarios

---
+++ function.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById('button-1').onclick=function () { 
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "No hay proyectos recientes"
+}
